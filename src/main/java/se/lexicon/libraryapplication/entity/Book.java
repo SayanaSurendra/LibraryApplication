d51dcd24350f7e26c22a881f084d78fbfad3889f
Sayana Surendran <sayanasurendran31@gmail.com>
2025-01-21 20:18:55 +0100

    Added entities

---
+++ src/main/java/se/lexicon/libraryapplication/entity/Book.java
@@ -0,0 +1,93 @@
+package se.lexicon.libraryapplication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String isbn;
+    private String title;
+    private int maxLoanDays;
+
+
+    public Book() {
+    }
+
+    public Book(int id, String isbn, String title, int maxLoanDays) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.maxLoanDays = maxLoanDays;
+    }
+
+
+    public Book(String isbn, String title, int maxLoanDays) {
+        this.isbn = isbn;
+        this.title = title;
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getMaxLoanDays() {
+        return maxLoanDays;
+    }
+
+    public void setMaxLoanDays(int maxLoanDays) {
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Book{");
+        sb.append("id=").append(id);
+        sb.append(", isbn='").append(isbn).append('\'');
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", maxLoanDays=").append(maxLoanDays);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && maxLoanDays == book.maxLoanDays && Objects.equals(isbn, book.isbn) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn, title, maxLoanDays);
+    }
+}
---
+++ src/main/java/se/lexicon/libraryapplication/entity/BookLoan.java
@@ -0,0 +1,124 @@
+package se.lexicon.libraryapplication.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class BookLoan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    LocalDate loanDate;
+
+    @Column(nullable = false)
+    LocalDate dueDate;
+
+    @Column(nullable = false)
+    boolean returned;
+
+    @ManyToOne
+    AppUser borrower;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    Book book;
+
+
+    public BookLoan() {
+    }
+
+    public BookLoan(LocalDate loanDate, LocalDate dueDate, boolean returned, AppUser borrower, Book book) {
+        this.loanDate = loanDate;
+        this.dueDate = dueDate;
+        this.returned = returned;
+        this.borrower = borrower;
+        this.book = book;
+    }
+
+    public BookLoan(int id, LocalDate loanDate, LocalDate dueDate, boolean returned, AppUser borrower, Book book) {
+        this.id = id;
+        this.loanDate = loanDate;
+        this.dueDate = dueDate;
+        this.returned = returned;
+        this.borrower = borrower;
+        this.book = book;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public AppUser getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(AppUser borrower) {
+        this.borrower = borrower;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isReturned() {
+        return returned;
+    }
+
+    public void setReturned(boolean returned) {
+        this.returned = returned;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("BookLoan{");
+        sb.append("id=").append(id);
+        sb.append(", loanDate=").append(loanDate);
+        sb.append(", dueDate=").append(dueDate);
+        sb.append(", returned=").append(returned);
+        sb.append(", borrower=").append(borrower);
+        sb.append(", book=").append(book);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BookLoan bookLoan = (BookLoan) o;
+        return id == bookLoan.id && returned == bookLoan.returned && Objects.equals(loanDate, bookLoan.loanDate) && Objects.equals(dueDate, bookLoan.dueDate) && Objects.equals(borrower, bookLoan.borrower) && Objects.equals(book, bookLoan.book);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, loanDate, dueDate, returned, borrower, book);
+    }
+}
